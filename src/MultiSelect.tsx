271d91b80caeb9dc09254314ab1a9473dc190b9d
Roman Chaban <roman.chaban1001@gmail.com>
2024-03-11 19:45:38 +0200

    create custom select

---
+++ src/MultiSelect.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { FC } from 'react';
+import './app.scss';
+import Select, { OnChangeValue } from 'react-select';
+import { useState } from 'react';
+import { IOption } from './app.interface';
+import makeAnimated from 'react-select/animated';
+
+const options: IOption[] = [
+  {
+    value: 'Ukraine',
+    label: 'Ukraine',
+  },
+  {
+    value: 'USA',
+    label: 'USA',
+  },
+  {
+    value: 'Spain',
+    label: 'Spain',
+  },
+];
+
+const animatedComponents = makeAnimated();
+
+const MultiSelect: FC = (): JSX.Element => {
+  const [currentCountries, setCurrentCountries] = useState(['USA', 'Ukraine']);
+
+  // const isMulti = true;
+
+  const getValue = () => {
+    return currentCountries
+      ? options.filter((option) => currentCountries.indexOf(option.value) >= 0)
+      : [];
+  };
+
+  const onChange = (newValue: OnChangeValue<IOption, boolean>) => {
+    setCurrentCountries((newValue as IOption[]).map((v: any) => v.value));
+  };
+
+  return (
+    <div className='select'>
+      <h1>Chose Country</h1>
+      <Select
+        classNamePrefix='custom-select'
+        value={getValue()}
+        onChange={onChange}
+        options={options}
+        components={animatedComponents}
+        isMulti
+        placeholder='Chose Country'
+      />
+    </div>
+  );
+};
+
+export default MultiSelect;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MultiSelect from './MultiSelect';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MultiSelect />
+  </React.StrictMode>
+);
+
